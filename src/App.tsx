e428c7b46f350e6dbc16d80159f804150859f24b
Paarth Madan <paarth.madan@shopify.com>
2021-07-20 11:35:23 -0400

    Install react, react-dom, scaffold basic App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function render() {
+  ReactDOM.render(
+    <h2>Seafoam GUI</h2>,
+    document.body
+  );
+}
+
+render();
