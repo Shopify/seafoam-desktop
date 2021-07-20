ee5f3dbabd8a87a6fb33fa380b2b4c2cde15d6a0
wildmaples <maple.ong@shopify.com>
2021-07-20 16:27:23 -0400

    Fix formatting according to prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import enTranslations from '@shopify/polaris/locales/en.json';
-import {AppProvider, Page, Card, ResourceList, TextStyle, Avatar} from '@shopify/polaris';
+import enTranslations from "@shopify/polaris/locales/en.json";
+import {
+  AppProvider,
+  Page,
+  Card,
+  ResourceList,
+  TextStyle,
+  Avatar,
+} from "@shopify/polaris";
 
 function render() {
   ReactDOM.render(
@@ -13,19 +20,19 @@ function render() {
             items={[
               {
                 id: 341,
-                url: 'customers/341',
-                name: 'Mae Jemison',
-                location: 'Decatur, USA',
+                url: "customers/341",
+                name: "Mae Jemison",
+                location: "Decatur, USA",
               },
               {
                 id: 256,
-                url: 'customers/256',
-                name: 'Ellen Ochoa',
-                location: 'Los Angeles, USA',
+                url: "customers/256",
+                name: "Ellen Ochoa",
+                location: "Los Angeles, USA",
               },
             ]}
             renderItem={(item) => {
-              const {id, url, name, location} = item;
+              const { id, url, name, location } = item;
               const media = <Avatar customer size="medium" name={name} />;
 
               return (
