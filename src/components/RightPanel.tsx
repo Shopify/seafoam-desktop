7a6ecee51c00b75a3745dfcdb27060a55ca0a8ef
Paarth Madan <paarth.madan@shopify.com>
2021-07-21 11:41:30 -0400

    Add sample GraphViz component

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 
-import { Page, TopBar } from "@shopify/polaris";
+import { Page, TopBar, Card } from "@shopify/polaris";
+import { Graphviz } from "graphviz-react";
+import { GraphvizOptions } from "d3-graphviz";
+
+import dot from "../data/sample_encoding_name.js";
 
 const RightPanel: React.FunctionComponent = () => {
   const [searchValue, setSearchValue] = React.useState("");
@@ -10,11 +14,27 @@ const RightPanel: React.FunctionComponent = () => {
     []
   );
 
+  const graphOptions: GraphvizOptions = {
+    width: null,
+    height: 1000,
+    fit: true,
+  };
+
   return (
     <Page title="Graph Panel">
       <TopBar.SearchField value={searchValue} onChange={onSearchValueChange} />
+      <Card>
+        <div style={box}>
+          <Graphviz dot={dot} options={graphOptions} />
+        </div>
+      </Card>
     </Page>
   );
+};
+
+const box = {
+  flex: 1,
+  height: "100%",
 };
 
 export default RightPanel;
