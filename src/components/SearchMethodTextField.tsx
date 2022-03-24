272162ada1bd5aea03d064beb01b261b8d844058
Kevin Menard <kevin.menard@shopify.com>
2022-03-24 10:20:13 -0400

    Convert the method search component from Polaris to Ant.

--- src/components/SearchMethodTextField.tsx
+++ src/components/SearchMethodTextField.tsx
@@ -1,33 +1,20 @@
-import { Button, TextField } from "@shopify/polaris";
-import React, { useState } from "react";
+import React from "react";
+import { Input } from "antd";
+
+const { Search } = Input;
 
 interface Props {
   methodFilter: (query: string) => void;
 }
 
 export default function SearchMethodTextField(props: Props) {
-  const [textFieldValue, setTextFieldValue] = useState("");
-  const handleTextFieldChange = React.useCallback(
-    (value) => setTextFieldValue(value),
-    []
-  );
-
-  function handleMethodSearch() {
-    props.methodFilter(textFieldValue);
-    window.logger.log(textFieldValue);
-  }
-
   return (
     <div>
-      <TextField
-        autoComplete="off"
-        label="Search Methods"
-        value={textFieldValue}
-        onChange={handleTextFieldChange}
+      <div style={{ marginBottom: "4px" }}>Search Methods:</div>
+      <Search
         placeholder="Example: String#[]"
+        onSearch={(value) => props.methodFilter(value)}
       />
-      <br />
-      <Button onClick={handleMethodSearch}>Search</Button>
     </div>
   );
 }
