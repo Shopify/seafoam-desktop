517e88ac87b9e572373b516b0ef8984078e50b37
Kevin Menard <kevin.menard@shopify.com>
2022-09-17 00:00:18 +0200

    Upgrade to React 18.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import LeftPanel from "./components/LeftPanel";
 import RightPanel from "./components/RightPanel";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { GraphsLoadedProvider } from "./contexts/GraphsLoadedContext";
 import { GraphDataSourceProvider } from "./contexts/GraphDataSourceContext";
-
-const App = () => {
-  // The App is tentatively split into two "sections". This is achieved by using two full-height divs, and using CSS flex
-  // to create a 2:1 ratio between the right and left panel.
-
-  return (
-    <div style={styles.app as React.CSSProperties}>
-      <ErrorBoundary>
-        <GraphsLoadedProvider>
-          <GraphDataSourceProvider>
-            <div style={styles.box as React.CSSProperties}>
-              <div style={styles.left as React.CSSProperties}>
-                <LeftPanel />
-              </div>
-              <div style={styles.right as React.CSSProperties}>
-                <RightPanel />
-              </div>
-            </div>
-          </GraphDataSourceProvider>
-        </GraphsLoadedProvider>
-      </ErrorBoundary>
-    </div>
-  );
-};
 
 const styles = {
   app: {
@@ -55,4 +31,26 @@ const styles = {
   },
 };
 
-ReactDOM.render(<App />, document.querySelector("#app"));
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+
+// The App is tentatively split into two "sections". This is achieved by using two full-height divs, and using CSS flex
+// to create a 2:1 ratio between the right and left panel.
+
+root.render(
+  <div style={styles.app as React.CSSProperties}>
+    <ErrorBoundary>
+      <GraphsLoadedProvider>
+        <GraphDataSourceProvider>
+          <div style={styles.box as React.CSSProperties}>
+            <div style={styles.left as React.CSSProperties}>
+              <LeftPanel />
+            </div>
+            <div style={styles.right as React.CSSProperties}>
+              <RightPanel />
+            </div>
+          </div>
+        </GraphDataSourceProvider>
+      </GraphsLoadedProvider>
+    </ErrorBoundary>
+  </div>
+);
